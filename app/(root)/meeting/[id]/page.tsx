c723e94940d97227eab427d7b98b29ae5abdcce4
Mohit Verma <mohit51341862@gmail.com>
2025-01-11 02:02:14 +0530

    meeting id

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,3 +1,54 @@
+// 'use client';
+
+// import { useState } from 'react';
+// import { useUser } from '@clerk/nextjs';
+// import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+// import { useParams } from 'next/navigation';
+// import { Loader } from 'lucide-react';
+
+// import { useGetCallById } from '@/hooks/useGetCallById';
+// import Alert from '@/components/Alert';
+// import MeetingSetup from '@/components/MeetingSetup';
+// import MeetingRoom from '@/components/MeetingRoom';
+
+// const MeetingPage = () => {
+//   const { id } = useParams();
+//   const { isLoaded, user } = useUser();
+//   const { call, isCallLoading } = useGetCallById(id);
+//   const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+//   if (!isLoaded || isCallLoading) return <Loader />;
+
+//   if (!call) return (
+//     <p className="text-center text-3xl font-bold text-white">
+//       Call Not Found
+//     </p>
+//   );
+
+//   // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+//   const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+//   if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+
+//   return (
+//     <main className="h-screen w-full">
+//       <StreamCall call={call}>
+//         <StreamTheme>
+
+//         {!isSetupComplete ? (
+//           <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+//         ) : (
+//           <MeetingRoom />
+//         )}
+//         </StreamTheme>
+//       </StreamCall>
+//     </main>
+//   );
+// };
+
+// export default MeetingPage;
+
+
 'use client';
 
 import { useState } from 'react';
@@ -14,7 +65,10 @@ import MeetingRoom from '@/components/MeetingRoom';
 const MeetingPage = () => {
   const { id } = useParams();
   const { isLoaded, user } = useUser();
-  const { call, isCallLoading } = useGetCallById(id);
+
+  // Ensure id is defined before calling the hook
+  const { call, isCallLoading } = id ? useGetCallById(id) : { call: null, isCallLoading: false };
+
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   if (!isLoaded || isCallLoading) return <Loader />;
@@ -34,12 +88,11 @@ const MeetingPage = () => {
     <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-
-        {!isSetupComplete ? (
-          <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-        ) : (
-          <MeetingRoom />
-        )}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
