f595f2a564984b8ec231b6c0fcbba4c80c0f3e13
Mohit Verma <118046944+mohit1106@users.noreply.github.com>
2025-01-31 09:52:26 +0530

    Update StreamClientProvider.tsx

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -3,7 +3,7 @@
 import { ReactNode, useEffect, useState } from 'react';
 import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
 import { useUser } from '@clerk/nextjs';
-
+// token provider
 import { tokenProvider } from '@/actions/stream.actions';
 import Loader from '@/components/Loader';
 
