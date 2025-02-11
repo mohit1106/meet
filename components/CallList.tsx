d784e594dac25bac71cbd98c7cc657b308fccb02
Mohit Verma <118046944+mohit1106@users.noreply.github.com>
2025-02-11 17:29:02 +0530

    Update CallList.tsx

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -66,36 +66,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 
   return (
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
-      {/* {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording, index) => (
-        <MeetingCard
-          key={(meeting as Call).id || `${type}-${index}`}
-          icon={
-            type === 'ended'
-              ? '/icons/previous.svg'
-              : type === 'upcoming'
-                ? '/icons/upcoming.svg'
-                : '/icons/recordings.svg'
-          }
-          title={
-            (meeting as Call).state?.custom?.description?.substring(0, 31) || meeting?.filename?.substring(0, 20) || 'Personal Meeting'
-          }
-          date={meeting.state?.startsAt.toLocaleString() || meeting.start_time.toLocaleString()}
-          isPreviousMeeting={type === 'ended'}
-          buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
-          buttonText={type === 'recordings' ? 'Play' : 'Start'}
-          handleClick={
-            type === 'recordings'
-              ? () => router.push(`${meeting.url}`)
-              : () => router.push(`/meeting/${meeting.id}`)
-          }
-          link={
-            type === 'recordings'
-              ? meeting.url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
-          }
-        />
-      )) : (
-        <h1>{noCallsMessage}</h1>
-      )} */}
+      {/*  */}
       {calls && calls.length > 0 ? (
         calls.map((meeting: Call | CallRecording) => (
           <MeetingCard
